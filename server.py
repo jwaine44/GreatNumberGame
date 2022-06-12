5145ee89a49c572ebbed4693415327f16609b304
jwaine44 <jwaine44@gmail.com>
2022-06-12 19:40:49 -0400

    Initialize Repo

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, session
+
+import random
+
+app = Flask(__name__)
+
+app.secret_key = 'I wanna tell you my secret'
+
+@app.route("/")
+def index():
+    if "num" not in session:
+        session['num'] = random.randint(1, 100)
+
+    return render_template("index.html")
+
+@app.route("/guess", methods=['POST'])
+def create_guess():
+    session['guess'] = int(request.form['guess'])
+    return redirect("/")
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
